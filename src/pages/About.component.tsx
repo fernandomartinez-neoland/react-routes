82bcc57701eb5c86aa92202f6e5c34543709dea2
altrios <famartinez.ti@gmail.com>
2026-02-19 17:09:52 +0100

    middleware

--- src/pages/About.component.tsx
+++ src/pages/About.component.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import axios from "../../middleware/axios.middleware.tsx";
 function About() {
-  return (
-    <div id="about">About</div>
-  )
+  useEffect(() => {
+    let data = JSON.stringify({ correo: "fer@correo.com" });
+
+    let config = {
+      method: "post",
+      url: "/finduser", // Ya no necesitas toda la URL si usaste baseURL
+      headers: { "Content-Type": "application/json" },
+      data: data,
+    };
+
+    // Usas 'api' en lugar de 'axios'
+    axios
+      .request(config)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return <div id="about">About</div>;
 }
 
-export default About
+export default About;
